88fb3e340933add070b4e4425fb2c55af85b9351
Lanqing Yuan <yuanlq@uchicago.edu>
2022-02-08 18:51:00 -0600

    Corrected AFT

--- sims1.py
+++ sims1.py
@@ -13,8 +13,78 @@ import matplotlib.pyplot as plt
 # Mean AFTs around certain z positions from Ar37 S1 data
 ZSLIACES = np.array([-128.20647559, -116.14423757, -104.08199954,  -92.01976151, -79.95752349,  
                      -67.89528546,  -55.83304744,  -43.77080941, -31.70857139,  -19.64633336])
-AFTS = np.array([0.11675146, 0.14507483, 0.17712556,0.21277627, 0.2502666,
+DATA_AFTS = np.array([0.11675146, 0.14507483, 0.17712556,0.21277627, 0.2502666,
                  0.29347506, 0.33834386, 0.3823964, 0.43201056, 0.47643015])
+XS = np.linspace(-64.18666666666667, 64.18666666666667, 30)
+YS = np.linspace(-64.18666666666667, 64.18666666666667, 30)
+ZS = np.linspace(-153.4992099, 6.526389900000001, 100)
+
+
+def make_map(map_file, fmt='pkl', method='WeightedNearestNeighbors'):
+    """Fetch and make an instance of InterpolatingMap based on map_file
+    Alternatively map_file can be a list of ["constant dummy", constant: int, shape: list]
+    return an instance of DummyMap"""
+
+    if isinstance(map_file, list):
+        assert map_file[0] == 'constant dummy', ('Alternative file input can only be '
+                                                 '("constant dummy", constant: int, shape: list')
+        return DummyMap(map_file[1], map_file[2])
+
+    elif isinstance(map_file, str):
+        if fmt is None:
+            fmt = parse_extension(map_file)
+
+        map_data = straxen.get_resource(map_file, fmt=fmt)
+        return straxen.InterpolatingMap(map_data, method=method)
+
+    else:
+        raise TypeError("Can't handle map_file except a string or a list")
+
+
+def parse_extension(name):
+    """Get the extention from a file name. If zipped or tarred, can contain a dot"""
+    split_name = name.split('.')
+    if len(split_name) == 2:
+        fmt = split_name[-1]
+    elif len(split_name) > 2 and 'gz' in name:
+        fmt = '.'.join(split_name[-2:])
+    else:
+        fmt = split_name[-1]
+    return 
+
+
+def get_sim_aft(z, pattern_map_file, fv_radius=60.73):
+    """Get the average AFT as a function of z from MC pattern map file.
+
+    Args:
+        z (float): depth (negative) in unit of cm
+        pattern_map_file (str): path to map. eg. '/dali/lgrandi/xenonnt/simulations/optphot/mc_v4.1.0/S1_1.63_0.99_0.99_0.99_0.99_5500_10000_30/XENONnT_S1_xyz_patterns_LCE_corrected_QEs_MCv4.1.0_wires.pkl' 
+        fv_radius (float, optional): Radius of fiducial volume. Defaults to 60.73 cm.
+
+    Returns:
+        avg_aft, std_aft (float): average and x-y standard deviation of AFT in map.
+    """
+    s1_pattern_map = make_map(map_file=pattern_map_file).data['map']
+
+    z_i = np.digitize(z, ZS)
+    aft_map = np.zeros((len(XS),len(YS)))
+    in_fv = np.zeros((len(XS),len(YS)), dtype=np.bool)
+
+    # For all x,ys in that z-slice, compute aft
+    for i, x in enumerate(XS):
+        for j, y in enumerate(YS):
+            if x**2 + y**2 <= fv_radius**2:
+                x_i = np.digitize(x, XS)
+                y_i = np.digitize(y, YS)
+                in_fv[i,j] = True
+                pattern = s1_pattern_map[x_i,y_i,z_i,:]
+                norm = 1/pattern.sum()
+                pattern *= norm
+                aft_map[i,j] = pattern[:253].sum()
+    
+    avg_aft = np.mean(aft_map[in_fv])
+    std_aft = np.std(aft_map[in_fv])
+    return avg_aft, std_aft
 
 
 def get_config(config_file='fax_config_nt_sr0_v0.json'):
@@ -23,7 +93,7 @@ def get_config(config_file='fax_config_nt_sr0_v0.json'):
     Args:
         config_file (str, optional): Configuration file name. Defaults to 'fax_config_nt_sr0_v0.json'.
     """
-    config = config = straxen.get_resource(config_file, config_file.split('.')[-1])
+    config = straxen.get_resource(config_file, config_file.split('.')[-1])
     return config
 
 
@@ -86,11 +156,13 @@ def interp_aft(z_position):
     Args:
         z_position (float): z position.
     """
-    f_aft = interp1d(ZSLIACES, AFTS)
+    f_aft = interp1d(ZSLIACES, DATA_AFTS)
     return f_aft(z_position)
 
 
-def propagation(z_position, spline_file='XENONnT_s1_proponly_va43fa9b_wires_20200625.json.gz'):
+def propagation(z_position, 
+                spline_file='XENONnT_s1_proponly_va43fa9b_wires_20200625.json.gz',
+                pattern_map_file='/dali/lgrandi/xenonnt/simulations/optphot/mc_v4.1.0/S1_1.69_0.99_0.99_0.99_0.99_10000_100_30/XENONnT_S1_xyz_patterns_LCE_corrected_QEs_MCv4.1.0_wires.pkl'):
     """Function gettting times from s1 timing splines. Weighted based on aft.
 
     Args:
@@ -100,7 +172,7 @@ def propagation(z_position, spline_file='XENONnT_s1_proponly_va43fa9b_wires_2020
     Returns:
         prop_t, prop_y (1darray): photon propagation as a function af time
     """
-    aft = interp_aft(z_position)
+    aft, _ = get_sim_aft(z=z_position, pattern_map_file=pattern_map_file)
     prop_t, prop_y_top = optical_propagation(top=True,  z_position=z_position, spline_file=spline_file)
     prop_t, prop_y_bot = optical_propagation(top=False, z_position=z_position, spline_file=spline_file)
 
@@ -171,6 +243,7 @@ def spe(config_file='fax_config_nt_sr0_v0.json'):
 
 def sims1(z_position, interaction_type, e_dep,
           spline_file='XENONnT_s1_proponly_va43fa9b_wires_20200625.json.gz', 
+          pattern_map_file = '/dali/lgrandi/xenonnt/simulations/optphot/mc_v4.1.0/S1_1.69_0.99_0.99_0.99_0.99_10000_100_30/XENONnT_S1_xyz_patterns_LCE_corrected_QEs_MCv4.1.0_wires.pkl',
           config_file='fax_config_nt_sr0_v0.json'):
     """Do as what wfsim does, give the S1 pulse shape of certain type interaction at certain depth.
 
@@ -179,9 +252,10 @@ def sims1(z_position, interaction_type, e_dep,
         interaction_type (int): Following the NEST type of intereaction.
         e_dep (float): energy deposit in unit of keV
         spline_file: pointer to s1 optical propagation splines from resources.
+        pattern_map_file (str): path to map. eg. '/dali/lgrandi/xenonnt/simulations/optphot/mc_v4.1.0/S1_1.63_0.99_0.99_0.99_0.99_5500_10000_30/XENONnT_S1_xyz_patterns_LCE_corrected_QEs_MCv4.1.0_wires.pkl' 
         config_file (str, optional): Configuration file name. Defaults to 'fax_config_nt_sr0_v0.json'.
     """
-    prop_t, prop_y = propagation(z_position=z_position, spline_file=spline_file)
+    prop_t, prop_y = propagation(z_position=z_position, spline_file=spline_file, pattern_map_file=pattern_map_file)
     scint_t, scint_y = scintillation(interaction_type=interaction_type, e_dep=e_dep, 
                                      config_file=config_file)
     tts_t, tts_y = transit(config_file=config_file)
@@ -196,6 +270,7 @@ def sims1(z_position, interaction_type, e_dep,
 
 def get_s1_templates(interaction_type, e_dep, z_positions=ZSLIACES,
                      spline_file='XENONnT_s1_proponly_va43fa9b_wires_20200625.json.gz', 
+                     pattern_map_file = '/dali/lgrandi/xenonnt/simulations/optphot/mc_v4.1.0/S1_1.69_0.99_0.99_0.99_0.99_10000_100_30/XENONnT_S1_xyz_patterns_LCE_corrected_QEs_MCv4.1.0_wires.pkl',
                      config_file='fax_config_nt_sr0_v0.json',
                      plot=True):
     """Get s1 templates based on wfsim from different depth slices.
@@ -205,6 +280,7 @@ def get_s1_templates(interaction_type, e_dep, z_positions=ZSLIACES,
         e_dep ([type]): [description]
         z_positions ([type], optional): [description]. Defaults to ZSLIACES.
         spline_file (str, optional): [description]. Defaults to 'XENONnT_s1_proponly_va43fa9b_wires_20200625.json.gz'.
+        pattern_map_file (str): path to map. eg. '/dali/lgrandi/xenonnt/simulations/optphot/mc_v4.1.0/S1_1.63_0.99_0.99_0.99_0.99_5500_10000_30/XENONnT_S1_xyz_patterns_LCE_corrected_QEs_MCv4.1.0_wires.pkl' 
         config_file (str, optional): [description]. Defaults to 'fax_config_nt_sr0_v0.json'.
         plot (bool, optional): [description]. Defaults to True.
     """
@@ -217,7 +293,9 @@ def get_s1_templates(interaction_type, e_dep, z_positions=ZSLIACES,
     sim_wfs = np.zeros((len(z_positions),800))
 
     for e in range(len(z_positions)):
-        y1 = sims1(z_position=z_positions[e], interaction_type=interaction_type, e_dep=e_dep)
+        y1 = sims1(z_position=z_positions[e], interaction_type=interaction_type, e_dep=e_dep, 
+                   spline_file=spline_file, pattern_map_file=pattern_map_file, 
+                   config_file=config_file)
         if plot:
             plt.plot(y1, c=colors_er(1+e*10) , linewidth=1, alpha=0.3)
         sim_wfs[e] = y1[100:900]
